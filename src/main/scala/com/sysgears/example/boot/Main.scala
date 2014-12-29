0df9a0dc425cfa7011d5ea10ba945c50ff9b486c
Andrey Litvinenko <drLitvinenko@gmail.com>
2014-12-29 18:04:36 +0200

    Fixed comments

--- src/main/scala/com/sysgears/example/boot/Main.scala
+++ src/main/scala/com/sysgears/example/boot/Main.scala
@@ -8,24 +8,24 @@ import com.sysgears.example.domain.{Chocolate, ChocolateQueryParams}
 object Main extends App {
   val chocolateDAO = new ChocolateDAO
 
-  //data for test:
+  // Data for test:
   val entry = Chocolate(name = "Chocolate santa", ingredients = "cacao, mushrooms, milk", producer = "Kind Chocolate")
   val updateEntry = entry.copy(producer = "Wonka's Chocolate Factory")
 
-  //create:
+  // Create:
   val id = chocolateDAO.insert(entry)
   assert(id.isDefined)
 
-  //read:
+  // Read:
   val getResult = chocolateDAO.findOneById(id.get)
   assert(getResult.isDefined)
   assert(getResult.get == entry.copy(_id = id.get))
 
-  //update:
+  // Update:
   chocolateDAO.update(ChocolateQueryParams(name = Some("Chocolate santa")), updateEntry)
   assert(chocolateDAO.find(ChocolateQueryParams(producer = Some("Wonka's Chocolate Factory"))).nonEmpty)
 
-  //delete:
+  // Delete:
   chocolateDAO.removeById(id.get)
   assert(chocolateDAO.findOneById(id.get).isEmpty)
 }
